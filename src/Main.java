424acdf1dd74404375696a8f0f313525ed264d3e
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-23 10:41:39 +0100

    Ejercicio con iterator y una lista de numeros

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> lista = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("Introduce un numero (Cero para salir): ");
+            int num = sc.nextInt();
+            lista.add(num);
+            if (num == 0) {
+                continuar = false;
+            }
+        }
+        Iterator<Integer> iterator = lista.iterator();
+        while (iterator.hasNext()) {
+            int valor = iterator.next();
+            if (valor % 2 == 0) {
+                iterator.remove();
+            }
+        }
+        System.out.println("Lista: " + lista);
+    }
 }
